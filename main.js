5a2bef5ae87ea73e48fa97382e7e32c07df8a4f1
Tanay Somisetty <Tanay@Tanays-MacBook-Air.local>
2020-08-05 15:21:49 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+window.onload=function(){
+	canv=document.getElementById("gc");
+	ctx=canv.getContext("2d");
+	score=document.getElementById("score");
+	highscore=document.getElementById("highscore")
+	document.addEventListener("keydown", keyPush);
+	setInterval(game,1000/15);
+}
+px=py=10;
+gs=tc=20;
+ax=ay=15;
+xv=yv=0;
+trail=[];
+tail = 5;
+sCounter = 0; //score counter
+hsCounter = 0; //highscore counter
+function game(){
+	
+	px+=xv;
+	py+=yv;
+	if(px<0){
+		px= tc-1;
+	}
+	if(px>tc-1){
+		px=0;
+	}
+	if(py<0){
+		py= tc-1;
+	}
+	if(py>tc-1){
+		py= 0;
+	}
+	ctx.fillStyle="black";
+	ctx.fillRect(0,0,canv.width,canv.height);
+
+	ctx.fillStyle="lime";
+	for(var i=0; i<trail.length; i++){
+		ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
+		if(trail[i].x==px && trail[i].y==py){
+			tail = 5;
+			sCounter = 0;
+			score.innerHTML = sCounter;
+		}
+	}
+	trail.push({x:px, y:py});
+	while(trail.length>tail){
+		trail.shift();
+	}
+
+	if(ax==px && ay==py){
+		tail++;
+		sCounter++;
+		score.innerHTML = sCounter;
+		if(sCounter > hsCounter){
+			hsCounter++;
+			highscore.innerHTML = hsCounter;
+		}
+		ax=Math.floor(Math.random()*tc);
+		ay=Math.floor(Math.random()*tc);
+	}
+
+	ctx.fillStyle="red";
+	ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2);
+}
+function keyPush(evt){
+	switch(evt.keyCode){
+		case 37:
+			xv=-1;yv=0;
+			break;
+		case 38:
+			xv=0;yv=-1;
+			break;
+		case 39:
+			xv=1;yv=0;
+			break;
+		case 40:
+			xv=0;yv=1;
+			break;
+	}
+}
